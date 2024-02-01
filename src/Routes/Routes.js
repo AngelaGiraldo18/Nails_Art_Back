b1215483c1244259d2fe1fc3869c4346556f483f
AngelaGiraldo18 <at593987@gmail.com>
2024-01-31 20:59:32 -0500

    Merge branch 'Hu/cambios_back' into main

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,14 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const verifyToken = require("../Middleware/verifyToken.js");
 const UsuriosControllers = require("../Controllers/UsuariosController.js");
+const Manicurista = require("../Controllers/Manicurista/ManicuristaControllers.js")
+const manicuristaController = require('../Controllers/Manicurista/ManicuristaControllers.js');
 
 router.get("/", (req, res) => {
     res.json({
         mensaje: "Bienvenido a la api de Nails art"
     });
 });
-
+//ruta para el usuario 
 router.post("/createusuario", UsuriosControllers.createUser);
+
+//Esta es la ruta para el crud de manicuristas 
+router.post("/createManicurista", Manicurista.createManicurista);
+router.get('/getmanicuristas',manicuristaController.getManicuristas)
+
 
 module.exports = router;
